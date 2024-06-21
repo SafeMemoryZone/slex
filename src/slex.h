adc8abebff643a7e8f3841c7dbc7204dd3b40979
Viliam Holly <viliamteoholly@gmail.com>
2024-06-21 15:24:35 +0200

    Fix some bugs and cleanup code

--- src/slex.h
+++ src/slex.h
@@ -31,8 +31,7 @@ typedef struct {
 // - stream_end: Pointer to the character just past the last character in the stream (or to EOF).
 // - string_store: Pointer to the storage used for parsing strings.
 // - string_store_len: Specifies the length of string_store.
-void slex_init_context(SlexContext *context, char *stream_start,
-                       char *stream_end, char* string_store, int string_store_len);
+void slex_init_context(SlexContext *context, char *stream_start, char *stream_end, char* string_store, int string_store_len);
 
 // Description:
 // - This function parses a token and advances context->parse_ptr.
@@ -49,8 +48,7 @@ int slex_get_next_token(SlexContext *context);
 // - stream_begin: Pointer to the location from where the lines and columns are counted.
 // - line_num: Output pointer for the line number.
 // - col_num: Output pointer for the column number.
-void slex_get_token_location(SlexContext *context, char *stream_begin, 
-                             int *line_num, int *col_num);
+void slex_get_token_location(SlexContext *context, char *stream_begin, int *line_num, int *col_num);
 
 // Description:
 // - This function returns the current location of the parsing point.
@@ -61,8 +59,7 @@ void slex_get_token_location(SlexContext *context, char *stream_begin,
 // - stream_begin: Pointer to the location from where the lines and columns are counted.
 // - line_num: Output pointer for the line number.
 // - col_num: Output pointer for the column number.
-void slex_get_parse_ptr_location(SlexContext *context, char *stream_begin, 
-                             int *line_num, int *col_num);
+void slex_get_parse_ptr_location(SlexContext *context, char *stream_begin, int *line_num, int *col_num);
 
 #ifdef SLEX_IMPLEMENTATION
 
@@ -73,7 +70,7 @@ void slex_get_parse_ptr_location(SlexContext *context, char *stream_begin,
 #define SLEX_END_IS_TOKEN 1
 #endif
 
-/* -- Helper Functions -- */
+/* -- Implementation -- */
 
 static int slex_is_numeric(char c) {
   return c >= '0' && c <= '9';
@@ -84,24 +81,18 @@ static int slex_is_oct(char c) {
 }
 
 static int slex_is_hex(char c) {
-  return slex_is_numeric(c) || (c >= 'A' && c <= 'F') 
-         || (c >= 'a' && c <= 'f');
+  return slex_is_numeric(c) || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f');
 }
 
 static int slex_hex_to_int(char c) {
-  if(slex_is_numeric(c))
-    return c - '0';
-  if(c >= 'A' && c <= 'F')
-    return c - 'A' + 10;
-  if(c >= 'a' && c <= 'f')
-    return c - 'a' + 10;
-
+  if(slex_is_numeric(c)) return c - '0';
+  if(c >= 'A' && c <= 'F') return c - 'A' + 10;
+  if(c >= 'a' && c <= 'f') return c - 'a' + 10;
   return -1;
 }
 
 static int slex_is_whitespace(char c) {
-  return c == ' ' || c == '\t' ||
-         c == '\n' || c == '\v' || 
+  return c == ' '  || c == '\t' || c == '\n' || c == '\v' || 
          c == '\f' || c == '\r';
 }
 
@@ -113,19 +104,17 @@ static int slex_return_err(SlexContext *ctx) {
 
 static int slex_return_eof(SlexContext *ctx) {
 #if SLEX_END_IS_TOKEN
-    ctx->tok_ty = SLEX_TOK_eof;
-    ctx->first_tok_char = ctx->stream_end;
-    ctx->last_tok_char = ctx->stream_end;
-    return 1;
+  ctx->tok_ty = SLEX_TOK_eof;
+  ctx->first_tok_char = ctx->stream_end;
+  ctx->last_tok_char = ctx->stream_end;
+  return 1;
 #else
-    return slex_return_err(ctx);
+  return slex_return_err(ctx);
 #endif
 }
 
 static int slex_mul_overflows_u64(unsigned long long a, unsigned long long b) {
-  if(a == 0 || b == 0)
-    return 0;
-
+  if(a == 0 || b == 0) return 0;
   return a > 0xFFFFFFFFFFFFFFFF / b;
 }
 
@@ -139,28 +128,27 @@ static int slex_parse_num(SlexContext *ctx) {
   char *end = ctx->parse_point;
   unsigned long long fact = 0;
   while(end < ctx->stream_end) {
-    if(!slex_is_numeric(*end))
-      break;
+    if(!slex_is_numeric(*end)) break;
+
     if(slex_mul_overflows_u64(fact, 10)) {
       ctx->parse_point = end;
       return slex_return_err(ctx);
     }
     end++;
-    if(fact != 0)
-      fact *= 10;
-    else
-      fact = 1;
+    if(fact != 0) fact *= 10;
+    else fact = 1;
   }
 
-  int len = end - ctx->parse_point;
   unsigned long long num = 0;
 
   for(char *it = ctx->parse_point; it < end; it++) {
-    if(slex_add_overflows_u64((*it - '0') * fact, num)) {
+    unsigned long long n = (*it - '0') * fact;
+
+    if(slex_add_overflows_u64(n, num)) {
       ctx->parse_point = it;
       return slex_return_err(ctx);
     }
-    num += (*it - '0') * fact;
+    num += n;
     fact /= 10;
   }
 
@@ -174,40 +162,35 @@ static int slex_parse_int_lit(SlexContext *ctx) {
   ctx->first_tok_char = ctx->parse_point;
   ctx->tok_ty = SLEX_TOK_int_lit;
 
-  if(*ctx->parse_point != '0')
+  if(*ctx->parse_point != '0') 
     return slex_parse_num(ctx);
 
   // the current char is the last one
-  if(ctx->parse_point >= ctx->stream_end - 1)
-    goto zero;
+  if(ctx->parse_point >= ctx->stream_end - 1) goto zero;
 
   // hexadecimals
   if(ctx->parse_point[1] == 'x' || ctx->parse_point[1] == 'X') {
-    ctx->parse_point += 2; // consume 0 and x
+    ctx->parse_point += 2; // consume 0x
 
-    if(ctx->parse_point >= ctx->stream_end)
+    if(ctx->parse_point >= ctx->stream_end) 
       return slex_return_err(ctx);
 
     char *end = ctx->parse_point;
     unsigned long long fact = 0;
 
     while(end < ctx->stream_end) {
-      if(!slex_is_hex(*end))
-        break;
+      if(!slex_is_hex(*end)) break;
+
       if(slex_mul_overflows_u64(fact, 16)) {
         ctx->parse_point = end;
         return slex_return_err(ctx);
       }
       end++;
-      if(fact != 0)
-        fact *= 16;
-      else
-        fact = 1;
+      if(fact != 0) fact *= 16;
+      else fact = 1;
     }
 
-    int len = end - ctx->parse_point;
-
-    if(!len)
+    if(end - ctx->parse_point == 0) 
       return slex_return_err(ctx);
 
     unsigned long long hex = 0;
@@ -234,28 +217,27 @@ static int slex_parse_int_lit(SlexContext *ctx) {
     unsigned long long fact = 0;
 
     while(end < ctx->stream_end) {
-      if(!slex_is_oct(*end))
-        break;
+      if(!slex_is_oct(*end)) break;
+
       if(slex_mul_overflows_u64(fact, 8)) {
         ctx->parse_point = end;
         return slex_return_err(ctx);
       }
       end++;
-      if(fact != 0)
-        fact *= 8;
-      else
-        fact = 1;
+      if(fact != 0) fact *= 8;
+      else fact = 1;
     }
 
-    int len = end - ctx->parse_point;
     unsigned long long oct = 0;
 
     for(char *it = ctx->parse_point; it < end; it++) {
-      if(slex_add_overflows_u64((*it - '0') * fact, oct)) {
+      unsigned long long o = (*it - '0') * fact;
+
+      if(slex_add_overflows_u64(o, oct)) {
         ctx->parse_point = it;
         return slex_return_err(ctx);
       }
-      oct += (*it - '0') * fact;
+      oct += o;
       fact /= 8;
     }
 
@@ -272,32 +254,50 @@ zero:
   return 1;
 }
 
-static int slex_parse_esc_seq(SlexContext *ctx) {
-  /* TODO: Add support for unicode escape sequences and allow 
-           octs to be larger than 255. */
+static long long slex_parse_esc_seq(SlexContext *ctx) {
+  // TODO: add support for unicode and utf-8 encoding
 
-  ctx->parse_point++; // skip \
+  ctx->parse_point++; // consume \
 
-  if(ctx->parse_point >= ctx->stream_end)
+  if(ctx->parse_point >= ctx->stream_end) 
     return slex_return_err(ctx) -1;
 
   if(ctx->parse_point > ctx->stream_end - 3)
     goto other_sequence;
 
-  if(slex_is_oct(*ctx->parse_point) && slex_is_oct(ctx->parse_point[1]) 
-     && slex_is_oct(ctx->parse_point[2])) {
-    int oct = 0;
-    int fact = 64;
-    for(char *it = ctx->parse_point; it < ctx->parse_point + 3; it++) {
-      oct += (*it - '0') * fact;
-      fact /= 8; 
+  if(slex_is_oct(*ctx->parse_point)) {
+    char *end = ctx->parse_point;
+    int fact = 0;
+    int i = 0;
+
+    while(end < ctx->stream_end) {
+      if(!slex_is_oct(*end) || i == 3) break;
+      end++; i++;
+      if(fact != 0) fact *= 8;
+      else fact = 1;
     }
-    ctx->parse_point += 3;
+
+    int oct = 0;
+
+    for(char *it = ctx->parse_point; it < end; it++) {
+      int o = (*it - '0') * fact;
+      oct += o;
+      fact /= 8;
+    }
+
+    ctx->parse_point = end;
     return oct;
   }
 
-  if(*ctx->parse_point == 'x' && slex_is_hex(ctx->parse_point[1]) 
-     && slex_is_hex(ctx->parse_point[2])) {
+  if(*ctx->parse_point == 'x') {
+    if(!slex_is_hex(ctx->parse_point[1])) {
+      ctx->parse_point++;
+      return slex_return_err(ctx) -1;
+    }
+    if(!slex_is_hex(ctx->parse_point[2])) {
+      ctx->parse_point += 2;
+      return slex_return_err(ctx) -1;
+    }
     int hex = slex_hex_to_int(ctx->parse_point[1]) * 16 + slex_hex_to_int(ctx->parse_point[2]); 
     ctx->parse_point += 3;
     return hex;
@@ -309,7 +309,6 @@ other_sequence:
     default: ctx->parse_point--; return slex_return_err(ctx) -1;
     case 'a': return '\a';
     case 'b': return '\b';
-    case 'e': return '\e';
     case 'f': return '\f';
     case 'n': return '\n';
     case 'r': return '\r';
@@ -319,67 +318,32 @@ other_sequence:
     case '\'': return '\'';
     case '"': return '"';
     case '?': return '\?';
-    case '0': return '\0';
   }
 }
 
-static int slex_parse_char_lit(SlexContext *ctx) {
-  ctx->first_tok_char = ctx->parse_point;
-  ctx->str_len = 1;
-  ctx->tok_ty = SLEX_TOK_char_lit;
-  ctx->parse_point++; // skip '
-  
-  if(ctx->parse_point >= ctx->stream_end)
-    return slex_return_err(ctx);
-
-  if(*ctx->parse_point == '\\') {
-    int seq = slex_parse_esc_seq(ctx);
-
-    if(seq == -1)
-      return slex_return_err(ctx);
-    
-    *ctx->string_store = seq;
-  }
-  else if(*ctx->parse_point == '\'')
-    return slex_return_err(ctx); // empty char lit not allowed
-  else {
-    *ctx->string_store = *ctx->parse_point;
-    ctx->parse_point++;
-  }
-
-  if(ctx->parse_point >= ctx->stream_end || *ctx->parse_point != '\'')
-    return slex_return_err(ctx);
-
-  ctx->last_tok_char = ctx->parse_point;
-  ctx->parse_point++;
-  
-  return 1;
-}
-
-static int slex_parse_str_lit(SlexContext *ctx) {
+static int slex_parse_char_or_str_lit(SlexContext *ctx) {
   int curr_str_len = 0;
 
-  ctx->tok_ty = SLEX_TOK_str_lit;
+  char delim = *ctx->parse_point;
+  ctx->tok_ty = delim == '"' ? SLEX_TOK_str_lit : SLEX_TOK_char_lit;
   ctx->first_tok_char = ctx->parse_point;
 
-  ctx->parse_point++; // skip "
-  
-  while(ctx->parse_point < ctx->stream_end) {
+  ctx->parse_point++; // consume " or '
 
-    if(*ctx->parse_point == '"') {
+  while(ctx->parse_point < ctx->stream_end) {
+    if(*ctx->parse_point == delim) {
       ctx->last_tok_char = ctx->parse_point;
       ctx->str_len = curr_str_len;
       ctx->parse_point++;
       return 1;
     }
 
-    if(curr_str_len > ctx->string_store_len)
+    if(curr_str_len > ctx->string_store_len) 
       return slex_return_err(ctx);
-    
+
     if(*ctx->parse_point == '\\') {
-      int c = slex_parse_esc_seq(ctx);
-      if(c == -1)
-        return slex_return_err(ctx);
+      long long c = slex_parse_esc_seq(ctx);
+      if(c == -1) return slex_return_err(ctx);
       ctx->string_store[curr_str_len] = c;
     }
     else {
@@ -393,7 +357,7 @@ static int slex_parse_str_lit(SlexContext *ctx) {
 }
 
 void slex_init_context(SlexContext *ctx, char *stream_start,
-                       char *stream_end, char* string_store, int string_store_len) {
+    char *stream_end, char* string_store, int string_store_len) {
   ctx->parse_point = stream_start;
   ctx->stream_end = stream_end;
   ctx->string_store = string_store;
@@ -403,11 +367,10 @@ void slex_init_context(SlexContext *ctx, char *stream_start,
 int slex_get_next_token(SlexContext *ctx) {
   if(ctx->parse_point >= ctx->stream_end) 
     return slex_return_eof(ctx);
-  
-  while(ctx->parse_point <= ctx->stream_end - 1) {
-   if(!slex_is_whitespace(*ctx->parse_point)) 
-     break;
-   ctx->parse_point++;
+
+  while(ctx->parse_point < ctx->stream_end) {
+    if(!slex_is_whitespace(*ctx->parse_point)) break;
+    ctx->parse_point++;
   }
 
   if(ctx->parse_point >= ctx->stream_end) 
@@ -415,46 +378,43 @@ int slex_get_next_token(SlexContext *ctx) {
 
   if(slex_is_numeric(*ctx->parse_point))
     return slex_parse_int_lit(ctx);
-  
+
   switch(*ctx->parse_point) {
     default: return slex_return_err(ctx);
-    case '"': return slex_parse_str_lit(ctx);
-    case '\'': return slex_parse_char_lit(ctx);
+    case '"': case '\'': return slex_parse_char_or_str_lit(ctx);
   }
 }
 
 void slex_get_token_location(SlexContext *ctx, char *stream_begin, int *line_num, int *col_num) {
   int ln = 1;
-  int col = 0;
+  int col = 1;
 
   for(char *it = stream_begin; it < ctx->first_tok_char; it++) {
-    if(*it == '\n') {
+    if (*it == '\n') {
       ln++;
       col = 1;
-    }
-    else {
+    } else {
       col++;
     }
   }
   *line_num = ln;
-  *col_num = col + 1;
+  *col_num = col;
 }
 
 void slex_get_parse_ptr_location(SlexContext *ctx, char *stream_begin, int *line_num, int *col_num) {
   int ln = 1;
-  int col = 0;
+  int col = 1;
 
   for(char *it = stream_begin; it < ctx->parse_point; it++) {
-    if(*it == '\n') {
+    if (*it == '\n') {
       ln++;
       col = 1;
-    }
-    else {
+    } else {
       col++;
     }
   }
   *line_num = ln;
-  *col_num = col + 1;
+  *col_num = col;
 }
 #endif // SLEX_IMPLEMENTATION
 #endif // SLEX_H
